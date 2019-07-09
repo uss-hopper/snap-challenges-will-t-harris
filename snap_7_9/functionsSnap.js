52a6acfe04407eef981941399ce3421a16be1e49
Will Harris <will.t.harris@protonmail.com>
2019-07-09 08:20:38 -0600

    refactor multiplyNumbers to arrow function

--- snap_7_9/functionsSnap.js
+++ snap_7_9/functionsSnap.js
@@ -1,6 +1,5 @@
 // Take two numbers and return their multiplied value
-multiplyNumbers = (num1, num2) => {
-	return num1 * num2;
-}
+multiplyNumbers = (num1, num2) => num1 * num2;
+
 
 concatenateStrings = (string1, string2) => `${string1} ${string2}`;
