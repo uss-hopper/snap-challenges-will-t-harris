54e80d86b6c4a93a00e3335c88006c9d26f88e95
Will Harris <will.t.harris@protonmail.com>
2019-07-11 08:05:39 -0600

    renamed folder for 7/9 challenge

--- snap-7-9/functionsSnap.js
+++ snap-7-9/functionsSnap.js
@@ -3,3 +3,7 @@ multiplyNumbers = (num1, num2) => num1 * num2;
 
 
 concatenateStrings = (string1, string2) => `${string1} ${string2}`;
+
+function concatenateStrings(string1, string2) {
+    return string1 + string2;
+}
