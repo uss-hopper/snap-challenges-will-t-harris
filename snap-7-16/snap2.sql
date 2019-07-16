6e47757343ec328fe35fc259985217de7706b628
Will Harris <will.t.harris@protonmail.com>
2019-07-16 08:58:55 -0600

    redo snap2.sql statement, still not convinced that it is correct

--- snap-7-16/snap2.sql
+++ snap-7-16/snap2.sql
@@ -1,1 +1,4 @@
-SELECT * FROM tweet WHERE tweetId = "0536faef082b454e9d444cdbe7887d7a" AND likeProfileId = tweetProfileId;
+SELECT tweet.tweetContent, profile.profileAtHandle
+FROM tweet INNER JOIN profile
+on tweet.tweetProfileId = profile.profileId
+WHERE tweet.tweetId = 0536faef082b454e9d444cdbe7887d7a;
