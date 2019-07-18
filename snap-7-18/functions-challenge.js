b3fe31cdf4b7c93cad7004914cc3da1a105d3e4a
Will Harris <will.t.harris@protonmail.com>
2019-07-18 08:38:46 -0600

    refactor conversion function to else if statements

--- snap-7-18/functions-challenge.js
+++ snap-7-18/functions-challenge.js
@@ -1,22 +1,14 @@
 function convertWeightToGrams(weight, abbreviation) {
 	if(abbreviation === 'lbs') {
 		return (weight * 453.5924) + ' grams';
-	}
-
-	if(abbreviation === 'oz') {
+	} else if(abbreviation === 'oz') {
 		return (weight * 38.34952) + ' grams';
-	}
-
-	if(abbreviation === 'kg') {
+	} else if(abbreviation === 'kg') {
 		return (weight * 1000) + ' grams';
-	}
-
-	if(abbreviation === 'g') {
-		return weight + ' grams';
-	}
-
-	if(abbreviation === 'mg') {
+	} else if(abbreviation === 'mg') {
 		return (weight * .001) + ' grams';
+	} else if(abbreviation === 'g') {
+		return weight + ' grams';
 	}
 }
 
