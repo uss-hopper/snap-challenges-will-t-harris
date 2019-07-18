590120330cd52e069eb56e36fcb2c17c3c9c274c
Will Harris <will.t.harris@protonmail.com>
2019-07-18 08:21:15 -0600

    find find sum function, had to change to <= in for condition so that the loop would check the value of num as well.

--- snap-7-18/functions-challenge.js
+++ snap-7-18/functions-challenge.js
@@ -22,11 +22,11 @@ function weightToGrams(weight, abbreviation) {
 
 
 function findSumOfPositiveFactors(num) {
-	for(let i = 0; i < num; i++) {
-		let sum = 0;
+	let sum = 0;
+	for(let i = 0; i <= num; i++) {
 		if(num % i === 0) {
 			sum += i;
 		}
-		return sum;
 	}
+	return sum;
 }
