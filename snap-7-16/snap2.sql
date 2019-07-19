b7f786cb1bcee4e1238ffe8fc6b5a8ea78769281
Will Harris <will.t.harris@protonmail.com>
2019-07-19 14:36:08 -0600

    add answer from class

--- snap-7-16/snap2.sql
+++ snap-7-16/snap2.sql
@@ -2,3 +2,8 @@ SELECT tweet.tweetContent, profile.profileAtHandle
 FROM tweet INNER JOIN profile
 on tweet.tweetProfileId = profile.profileId
 WHERE tweet.tweetId = 0536faef082b454e9d444cdbe7887d7a;
+
+-- ANSWER FROM CLASS
+SELECT tweet.tweetContent, profile.profileAtHandle FROM tweet
+INNER JOIN `like` ON tweet.tweetId = like.likeTweedId
+INNER JOIN profile ON like.likeProfileId = profile.profileId;
