57ce5cc77c0e73d1bec51a7bb3a6ffbe27e3bd8e
Will Harris <will.t.harris@protonmail.com>
2019-07-30 08:20:33 -0600

    add Tweet class, docblock for method, query template

--- snap-7-30/Tweet.php
+++ snap-7-30/Tweet.php
@@ -1,19 +1,24 @@
 <?php
 
-/**
- * this method gets all tweets from a given date
- *
- * @param DateTime $tweetDate
- * @return SplFixedArray
- **/
-public function getTweetsByTweetDate(\DateTime $tweetDate) : \SplFixedArray {
-	// check to make sure date is valid
-	try {
-		$tweetDate = self::ValidateDate($tweetDate);
-	} catch(\InvalidArgumentException | \RangeException $exception) {
-		$exceptionType = get_class($exception);
-		throw(new $exceptionType($exception->getMessage(), 0, $exception));
-	}
+class Tweet {
+	/**
+	 * method to return all tweets on a given date
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param \DateTime $tweetDate date to get tweets for
+	 * @return \SplFixedArray SplFixedArray of tweets found
+	 */
+	public function getTweetsByTweetDate(\PDO $pdo, \DateTime $tweetDate) : \SplFixedArray {
+		// check to make sure date is valid
+		try {
+			$tweetDate = self::ValidateDate($tweetDate);
+		} catch(\InvalidArgumentException | \RangeException $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
 
-	// create query template
+		// create query template
+		$query = "SELECT tweetContent FROM tweet WHERE tweetDate = :tweetDate";
+		$statement = $pdo->prepare($query);
+	}
 }
