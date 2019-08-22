768d361ce3dc2838cfd00c8090fcf0db5e483353
Will Harris <will.t.harris@protonmail.com>
2019-08-22 08:09:58 -0600

    define reducer function, use array.reduce to sum all the numbers

--- snap-8-22/script.js
+++ snap-8-22/script.js
@@ -1,1 +1,6 @@
 const numArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+const numArraySum = numArray.reduce(reducer);
+console.log(numArraySum);
