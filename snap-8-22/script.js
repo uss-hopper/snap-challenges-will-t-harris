4c13c33b397a411b82eeeff4fd6aa73b2aa61399
Will Harris <will.t.harris@protonmail.com>
2019-08-22 08:18:25 -0600

    refactor to use es6 string literal syntax

--- snap-8-22/script.js
+++ snap-8-22/script.js
@@ -6,6 +6,6 @@ const numArraySum = numArray.reduce((accumulator, currentValue) => accumulator +
 
 const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
 
-const sentenceFromWordArray = wordArray.reduce((accumulator, currentValue) => accumulator + " " + currentValue);
+const sentenceFromWordArray = wordArray.reduce((accumulator, currentValue) => `${accumulator} ${currentValue}`);
 
 console.log(sentenceFromWordArray);
