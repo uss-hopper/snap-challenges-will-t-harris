037363a58053c1019f6dfc7c81889151fe9a6a61
Will Harris <will.t.harris@protonmail.com>
2019-08-22 08:16:32 -0600

    turned array into string using reduce

--- snap-8-22/script.js
+++ snap-8-22/script.js
@@ -4,4 +4,8 @@ const numArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 15
 
 const numArraySum = numArray.reduce((accumulator, currentValue) => accumulator + currentValue);
 
-console.log(numArraySum);
+const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
+
+const sentenceFromWordArray = wordArray.reduce((accumulator, currentValue) => accumulator + " " + currentValue);
+
+console.log(sentenceFromWordArray);
