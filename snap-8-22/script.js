ebd55bf5b37b7f9fa3fac48ee90c663bf3dbc8e7
Will Harris <will.t.harris@protonmail.com>
2019-08-27 08:16:46 -0600

    write pseudocode solution for first sample interview question

--- snap-8-22/script.js
+++ snap-8-22/script.js
@@ -1,8 +1,8 @@
 const numArray = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811];
 
-// const reducer = (accumulator, currentValue) => accumulator + currentValue;
-
 const numArraySum = numArray.reduce((accumulator, currentValue) => accumulator + currentValue);
+
+console.log(numArraySum);
 
 const wordArray = ["Deflector", "power", "at", "maximum.", "Energy", "discharge", "in", "six", "seconds.", "Warp", "reactor", "core", "primary", "coolant", "failure.", "Fluctuate", "phaser", "resonance", "frequencies.", "Resistance", "is", "futile.", "Recommend", "we", "adjust", "shield", "harmonics", "to", "the", "upper", "EM", "band", "when", "proceeding.", "These", "appear", "to", "be", "some", "kind", "of", "power-wave-guide", "conduits", "which", "allow", "them", "to", "work", "collectively", "as", "they", "perform", "ship", "functions.", "Increase", "deflector", "modulation", "to", "upper", "frequency", "band."];
 
