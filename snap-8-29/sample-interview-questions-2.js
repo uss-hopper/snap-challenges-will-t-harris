a64bc1c9be19834ed11b8df19e9622a545f8ff98
Will Harris <will.t.harris@protonmail.com>
2019-08-29 08:21:18 -0600

    add solution for question 2

--- snap-8-29/sample-interview-questions-2.js
+++ snap-8-29/sample-interview-questions-2.js
@@ -1,12 +1,24 @@
-# 1 - In pseudocode, write a function that takes a string and returns it with all vowels _(not including y)_ removed.
+// 1 - In pseudocode, write a function that takes a string and returns it with all vowels _(not including y)_ removed.
 
 function removeVowels(string) {
-    const result = '';
+    let result = '';
     const sentenceArray = string.split('');
     sentenceArray.map(character => {
-        if(character.toLowerCase() !== ('a' || 'e' || 'i' || 'o' || 'u') {
+        if(character.toLowerCase() !== ('a' || 'e' || 'i' || 'o' || 'u')) {
             result += character;
         }
         return result;
     });
+}
+
+// 2 - In pseudocode, write a function that takes an array of numbers and returns the greatest number in the array.
+
+function returnGreatestNumber(array) {
+    let greatestNumber = 0;
+    array.map(number => {
+        if(number > greatestNumber) {
+            greatestNumber = number;
+        }
+        return greatestNumber;
+    })
 }
