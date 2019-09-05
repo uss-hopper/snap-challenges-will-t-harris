d15d353d75573db85d71c9ffccdc8c560d33f931
Will Harris <will.t.harris@protonmail.com>
2019-09-05 08:54:45 -0600

    make get call to axios, store response.data in dogs variable, grab target element and store in variable

--- snap-9-5/script.js
+++ snap-9-5/script.js
@@ -1,2 +1,7 @@
 const axios = require('axios');
 
+axios.get(' https://dog.ceo/api/breeds/list/all ').then(response => {
+    const dogs = response.data;
+});
+
+const element = document.getElementById('target');
